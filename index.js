78fc4ccc90dd9520a9601101d71975101b460c64
norahkibathi <norah.njeri@student.moringaschool.com>
2024-01-19 03:57:20 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.splice(-1)
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.splice(0,1)
+}
+
+function appendCat(name) {
+  return [...cats, name]
+}
+
+function prependCat(name) {
+  return [name, ...cats]
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+  return cats.slice(1)
+}// Write your solution here!
